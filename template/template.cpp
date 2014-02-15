0dacc8e6e2728a3df708fa5a0b5bdc8b038bc328
mahesh <mahesh@mahesh-HP-Z420-Workstation.(none)>
2014-02-15 16:04:02 +0530

    added file copy script

---
+++ template/template.cpp
@@ -0,0 +1,15 @@
+/*
+* Template for Basic Puzzle Problems
+* @author: mahesh.msg.24
+* @date: 15/FEB/14
+* @version: 1.0
+*/
+
+#include<cstdio>
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+}
